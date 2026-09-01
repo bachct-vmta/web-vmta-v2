d426709234c3fc660fbf1200e1e7fd058debd8f3
bachbcbt <bachbcbt@gmail.com>
2026-09-01 20:01:25 +0700

    Perf: Enable Next.js Standalone Output to compress build footprint below 200MB

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   outputFileTracingRoot: path.resolve(__dirname),
   images: {
     unoptimized: true,
